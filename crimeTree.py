a3498b30ef011c5f60e412794ae60a50c9ba18b9
Nicholas Rucker <nick@115.185.21.198.tigernet.wifi.dyn.clemson.edu>
2019-05-01 13:00:51 -0400

    Added decision tree ml This is just the first attempt I am not too happy with the results, but I will try things out with other models and see what happens

---
+++ crimeTree.py
@@ -0,0 +1,19 @@
+import pandas as pd  
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+dataSet = pd.read_csv("cleanData/cleanedCrimes.csv")
+
+data = dataSet.iloc[:, [9, 10, 11, 12, 13, 14, 20, 21, 25, 27, 29, 30]]
+print(data)
+target = dataSet.iloc[:, 5].values
+
+dataTrain, dataTest, targetTrain, targetTest = train_test_split(data, target, test_size = .2)
+decisionTreeMachine = tree.DecisionTreeClassifier(criterion="gini")
+decisionTreeMachine.fit(dataTrain, targetTrain)
+
+predictions = decisionTreeMachine.predict(dataTest)
+
+print(accuracy_score(targetTest, predictions))
+
