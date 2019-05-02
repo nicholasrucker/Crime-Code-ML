1012637af04eee0f4378278263549f9088fceee1
Nicholas Rucker <nick@107.190.21.198.tigernet.wifi.dyn.clemson.edu>
2019-05-02 19:53:05 -0400

    Updated the KNN machine. Currently I am testing to find the optimal number of clusters

--- crimeKNN.py
+++ crimeKNN.py
@@ -9,22 +9,21 @@ dataSet = pd.read_csv("cleanData/cleanedCrimes.csv")
 data = dataSet.iloc[:, [9, 10, 12, 13, 14, 20, 21, 25, 31]]
 target = dataSet.iloc[:, 5].values
 
+i = 0
+
 for value in target:
 	value = str(value)
-	value = re.sub('[^0-9]','', value)
+	target[i] = re.sub('[^0-9]','', value)
+	i = i + 1
 
 dataTrain, dataTest, targetTrain, targetTest = train_test_split(data, target, test_size = .2)
 
-for i in range (1, 10):
+for i in range (1, 30):
 	print("With", i, "clusters: ")
 	
 	knn = KNeighborsClassifier(n_neighbors = i)
 	knn.fit(dataTrain, targetTrain)
 	predictions = knn.predict(dataTest)
-
-	predictions = str(predictions)
-	predictions = re.sub('[^0-9]','', predictions)
-	print(predictions)
 
 	print(accuracy_score(targetTest, predictions))
 
